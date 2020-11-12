5dab7bdc686bd64a77e982c6a0bdbcd5c15ed547
Teebreezy101 <tammy.batubo@gmail.com>
2020-11-12 23:59:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer fa3f24999b13d7c6b30c2fb1b0be25873c634e80");
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({"query":"query { viewer { avatarUrl,name, login,bio, followers {totalCount} following {totalCount} starredRepositories {totalCount }email twitterUsername repositories(last: 20) { totalCount nodes {name,stargazerCount,description,updatedAt, forkCount, primaryLanguage { name } }}}}"});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+let githubResults;
+
+fetch("https://api.github.com/graphql", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+      githubResults = result.data.viewer;
+      console.log(githubResults)
+  } )
+  .catch(error => console.log('error', error));
+
+//   format date
+// const d = new Date('');
+// d.getDay(): format(d.getMonth())
+// const month = d.toLocalString('default', {month: 'short'})
